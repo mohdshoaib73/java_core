3cc10e3b120e2ebd12475610bf6cb7c216b37951
Mohammad Shoaib <mohdshoaibm487@gmail.com>
2026-02-21 18:37:23 +0530

    day2 data types

---
+++ day2/First.java
@@ -0,0 +1,33 @@
+public class First {
+    public static void main(String[] args)
+    {
+        //Integers--> byte,short,int,long
+
+        byte a=5;
+        short b=55;
+        int c=10000;
+        long d=54515456;
+
+        System.out.println("values-->"+","+a+ ","+b+","+c+","+d);
+
+        System.out.println();
+
+        //Real numbers-->float,double
+        float f=5.614f;
+        double e=5.9999;
+
+        System.out.println("Floating number-->"+f+","+e);
+
+        //characters-->char
+
+        char ch='d';
+        System.out.println();
+        System.out.println("Charcters is -->"+ch);
+
+        //boolean--> boolean
+
+        boolean bl=true;
+        System.out.println();
+        System.out.println("bool value is -->"+bl);
+    }
+}
